f1d1be4a6ff8ffa3e3300572c5d06a1baeb3e7f9
Protosevich <protosevich2001@gmail.com>
2020-02-16 12:52:30 +0300

    feat: solved task 'dream-team'

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!(members instanceof Array)){
+    return false;
+  }
+  let result = '';
+  for(let i = 0; i < members.length; i++){
+    if(typeof(members[i]) == "string"){
+      let j = 0;
+      while (!/^[a-z]$/i.test(members[i][j])){
+        j++;
+      }
+      result = result + members[i][j].toUpperCase();
+    }
+  }
+  return result.split('').sort().join('');
 };

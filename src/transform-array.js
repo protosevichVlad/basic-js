82a1a22099fe6c7f50440d5d64a014880373f5bd
Protosevich <protosevich2001@gmail.com>
2020-02-19 23:56:20 +0300

    fix: fix 'transform-array' and 'what-season'

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)){
+        throw Error();
+    }
+    let result = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == '--discard-next'){
+            i += 2;
+            continue;
+        }else if(arr[i] == '--discard-prev'){
+            result.pop();
+        }else if(arr[i] == '--double-next'){
+            result.push(arr[i + 1]);
+        }else if(arr[i] == '--double-prev'){
+            result.push(arr[i - 1]);
+        }else result.push(arr[i]);
+    }
+    return result;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,9 +1,9 @@
 module.exports = function getSeason(date) {
-  if(date == undefined){
-    return 'Unable to determine the time of year!';
-  }
   if(!date instanceof Date){
-    throw Error;
+    throw Error();
+  }
+  if(date == undefined || (date - new Date()) == 0){
+    return 'Unable to determine the time of year!';
   }
   if(11 == date.getMonth() || date.getMonth() <= 1){
     return 'winter';

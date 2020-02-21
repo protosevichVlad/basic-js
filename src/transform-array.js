5d1b778ea91beed93b11ccd086f2e11d40f7c90b
Protosevich <protosevich2001@gmail.com>
2020-02-21 20:40:10 +0300

    fix: full solution transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,18 +2,26 @@ module.exports = function transform(arr) {
     if(!Array.isArray(arr)){
         throw Error();
     }
+    // console.log(arr);
     let result = [];
     for(let i = 0; i < arr.length; i++){
         if(arr[i] == '--discard-next'){
-            i += 2;
+            if(arr.length - 1 == i) continue;
+            i += 1;
             continue;
         }else if(arr[i] == '--discard-prev'){
+            if(i == 0) continue;
             result.pop();
         }else if(arr[i] == '--double-next'){
+            if(i == arr.length - 1) continue;
             result.push(arr[i + 1]);
         }else if(arr[i] == '--double-prev'){
+            if(i == 0) continue;
             result.push(arr[i - 1]);
         }else result.push(arr[i]);
     }
     return result;
 };
+
+
+// console.log(transform([NaN, false, false]));

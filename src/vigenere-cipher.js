c67b50a12dfa309bda95408984ef2624c3877f69
Protosevich <protosevich2001@gmail.com>
2020-02-23 14:49:36 +0300

    feat: full solution vigenere-cipter

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,53 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    flag = true;
+
+    constructor(arg=true){
+        this.flag = arg
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key) {
+        if(str === undefined || key === undefined){
+            throw Error();
+        }
+        if(!this.flag){
+            str = str.split('').reverse().join('');
+        }
+        let counter_no_letter = 0;
+        let result = '';
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        for(let i = 0; i < str.length; i++){
+            if(/^[a-z]$/i.test(str[i])) result += String.fromCharCode((str.charCodeAt(i) - 130 + key.charCodeAt((i - counter_no_letter) % key.length)) % 26 + 65);
+            else{
+                counter_no_letter++;
+                result += str[i];
+            } 
+        }
+        return result;
+    }
+
+    decrypt(str, key) {
+        if(str === undefined || key === undefined){
+            throw Error();
+        }
+        if(!this.flag){
+            str = str.split('').reverse().join('');
+        }
+        let counter_no_letter = 0;
+        let result = '';
+        str = str.toUpperCase();
+        key = key.toUpperCase();
+        for(let i = 0; i < str.length; i++){
+            if(/^[a-z]$/i.test(str[i])) result += String.fromCharCode((str.charCodeAt(i) + 26 - key.charCodeAt((i - counter_no_letter) % key.length)) % 26 + 65);
+            else{
+                counter_no_letter++;
+                result += str[i];
+            } 
+        }
+        return result;
     }
 }
 
 module.exports = VigenereCipheringMachine;
+// let a = new VigenereCipheringMachine();
+// console.log(a.encrypt('attack at dawn!', 'alphonse'))
